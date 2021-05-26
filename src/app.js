6859c2df0056ef6b5a1a13b02a890df7a7fb208c
apolopino <apolo.pino@gmail.com>
2021-05-26 00:27:37 +0000

    Javascript random string generation from pool

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let rand1 = Math.floor(Math.random() * who.length);
+  let rand2 = Math.floor(Math.random() * action.length);
+  let rand3 = Math.floor(Math.random() * what.length);
+  let rand4 = Math.floor(Math.random() * when.length);
+
+  //Aca insertar las excusas aleatorias
+  this.document.querySelector("#excusa").innerHTML =
+    who[rand1] + " " + action[rand2] + " " + what[rand3] + " " + when[rand4];
+
+  //probando
+  //this.document.querySelector("#cambiante").innerHTML = "<p>Holi ql</p>";
 };
